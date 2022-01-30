ad708c467d1b397cd80f4dd791ba3724e93ec157
Cybernetic Ransomware <aleksander.marszakiewicz.pp@gmail.com>
2022-01-30 23:39:53 +0100

    input Scan()

---
+++ input_testing/input_testing.go
@@ -0,0 +1,20 @@
+package main
+
+
+import (
+	"fmt"
+)
+
+
+func main() {
+	fmt.Println("Start")
+
+	var winnery_phrase string
+
+	fmt.Scan(&winnery_phrase)
+
+	fmt.Printf("Your phrase: %s", winnery_phrase)
+	fmt.Scan()
+
+
+}
